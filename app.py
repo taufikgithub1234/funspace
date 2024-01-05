e50621647f360d29e3825ccba2c3ba85ce7d9fb7
taufikgithub1234 <pikopik851@gmail.com>
2024-01-05 14:56:24 +0800

    Projek pertama github

---
+++ app.py
@@ -0,0 +1,8 @@
+# hitung luas segitiga sederhana
+def luas_segitiga():
+    a = int (input("masukkan alas segitiga"))
+    t = int (input("masukkan tinggi segitiga"))
+    luas = a * t /2
+    print("luas segitiga adlah :, luas")
+
+luas_segitiga()
